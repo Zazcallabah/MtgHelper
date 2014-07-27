8fe0989d765d427c2c61cb6bdf4de8924da27d45
Peter Hamberg <zazcallabah@gmail.com>
2014-07-27 09:31:55 +0200

    first poc, can we screenshot?

---
+++ MtgHelper/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace MtgHelper
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public void Click( object sender, EventArgs e )
+		{
+			ScreenCapture sc = new ScreenCapture();
+			// capture entire screen, and save it to a file
+			System.Drawing.Image img = sc.CaptureScreen();
+			img.Save( "test.png", ImageFormat.Png );
+		}
+	}
+}
