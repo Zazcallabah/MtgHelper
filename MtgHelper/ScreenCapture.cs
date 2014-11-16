2352cf3f97782b4a06d282b507b4683db294de6b
Peter Hamberg <zazcallabah@gmail.com>
2014-11-16 10:27:55 +0100

    Implemented screencaptureengine according to notes

--- MtgHelper/ScreenCapture.cs
+++ MtgHelper/ScreenCapture.cs
@@ -10,24 +10,25 @@ namespace MtgHelper
 	/// </summary>
 	public class ScreenCapture
 	{
-		public Image CaptureScreen()
+		public IntPtr GetHandle()
 		{
-			var foregroundWindowsHandle = User32.GetForegroundWindow();
+			return User32.GetForegroundWindow();
+		}
+
+		public Bitmap CaptureScreen( IntPtr windowHandle )
+		{
 			var rect = new User32.Rect();
-			User32.GetWindowRect( foregroundWindowsHandle, ref rect );
+			User32.GetWindowRect( windowHandle, ref rect );
 			var bounds = new Rectangle( rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top );
-
-
+			if( bounds.Width == 0 || bounds.Height == 0 )
+				return null;
 			var result = new Bitmap( bounds.Width, bounds.Height );
-
 			using( var g = Graphics.FromImage( result ) )
 			{
 				g.CopyFromScreen( new Point( bounds.Left, bounds.Top ), Point.Empty, bounds.Size );
 			}
-
 			return result;
 		}
-
 
 		/// <summary>
 		/// Helper class containing User32 API functions
@@ -48,7 +49,6 @@ namespace MtgHelper
 
 			[DllImport( "user32.dll" )]
 			public static extern IntPtr GetForegroundWindow();
-
 		}
 	}
 }
---
+++ MtgHelper/ScreenShotEngine.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+
+namespace MtgHelper
+{
+	public enum RelevantWindows
+	{
+		Main,
+		Play,
+		Chat,
+		Draft,
+	}
+
+	public class ImageStore
+	{
+		public ImageStore( Image bitmap, long crc, RelevantWindows window )
+		{
+			Image = bitmap;
+			Crc = crc;
+			Window = window;
+		}
+
+		public RelevantWindows Window { get; set; }
+		public long Crc { get; set; }
+		public Image Image { get; set; }
+	}
+	public class HandleStore
+	{
+		public HandleStore( IntPtr handleCandidate, RelevantWindows window )
+		{
+			Ptr = handleCandidate;
+			Window = window;
+		}
+
+		public IntPtr Ptr { get; set; }
+		public RelevantWindows Window { get; set; }
+	}
+
+	public class ScreenShotEngine
+	{
+		const int WaitPeriod = 1000;
+		readonly ScreenCapture _sc;
+		readonly Dictionary<long, RelevantWindows> _crcLookup = new Dictionary<long, RelevantWindows>
+		{
+			{559831290,RelevantWindows.Main}
+		};
+		readonly List<ImageStore> _images = new List<ImageStore>();
+		readonly List<HandleStore> _relevantHandles = new List<HandleStore>();
+		Thread _worker;
+		bool _continue;
+
+
+		public ScreenShotEngine()
+		{
+			_sc = new ScreenCapture();
+		}
+
+		public void Start()
+		{
+			_continue = true;
+			_worker = new Thread( Loop );
+			_worker.Start();
+		}
+
+		void Loop()
+		{
+			while( _continue )
+			{
+
+				var handleCandidate = _sc.GetHandle();
+				if( _relevantHandles.All( r => r.Ptr != handleCandidate ) )
+				{
+					var test = _sc.CaptureScreen( handleCandidate );
+					if( test != null )
+					{
+						//this cut may have to be something different
+						var cut = test.Cut( 0, 0, 35, 24 );
+						var crc = cut.Crc32();
+
+
+						if( _crcLookup.ContainsKey( crc ) )
+						{
+							var window = _crcLookup[crc];
+							Test( test, window );
+							_relevantHandles.Add( new HandleStore( handleCandidate, window ) );
+						}
+					}
+				}
+				var badHandles = new List<HandleStore>();
+				foreach( var h in _relevantHandles )
+				{
+					try
+					{
+						var img = _sc.CaptureScreen( h.Ptr );
+						if( img != null )
+							Test( img, h.Window );
+					}
+					catch( InvalidOperationException )
+					{
+						badHandles.Add( h );
+					}
+				}
+
+				foreach( var h in badHandles )
+				{
+					_relevantHandles.Remove( h );
+				}
+
+				Wait( WaitPeriod );
+			}
+		}
+
+		void Test( Bitmap b, RelevantWindows window )
+		{
+			var crc = b.Crc32();
+			var store = _images.FirstOrDefault( i => i.Window == window );
+			if( store != null )
+			{
+				if( store.Crc != crc )
+				{
+					store.Crc = crc;
+					store.Image = b;
+					InvokeImageChanged( window );
+				}
+			}
+			else
+			{
+				_images.Add( new ImageStore( b, crc, window ) );
+				InvokeImageChanged( window );
+			}
+		}
+
+		void Wait( int ms )
+		{
+			var ts = TimeSpan.FromMilliseconds( ms );
+			var mark = DateTime.Now;
+			while( mark + ts > DateTime.Now && _continue )
+			{
+				Thread.Sleep( 50 );
+			}
+		}
+
+		public void Stop()
+		{
+			_continue = false;
+		}
+
+		void InvokeImageChanged( RelevantWindows window )
+		{
+			Application.Current.Dispatcher.Invoke( delegate
+			{
+				if( !_continue )
+					return;
+				FireImageChanged( window );
+			} );
+		}
+
+		public event EventHandler<ImageChangedEventArgs> ImageChanged;
+
+		protected virtual void FireImageChanged( RelevantWindows window )
+		{
+			if( ImageChanged != null )
+				ImageChanged( this, new ImageChangedEventArgs( window ) );
+		}
+	}
+
+	public class ImageChangedEventArgs
+	{
+		public ImageChangedEventArgs( RelevantWindows window )
+		{
+			Window = window;
+		}
+		public RelevantWindows Window { get; private set; }
+	}
+}
